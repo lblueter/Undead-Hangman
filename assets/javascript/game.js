3293321e13be953c901ccdf2f10c038b99b6244a
lblueter <logan11023@yahoo.com>
2018-08-16 15:02:38 -0400

    make it check key press

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 var wins = document.getElementById("wins")
 var guessRemain = document.getElementById("guessRemain")
-var guessed = document.getElementById("guessed")
 var wordList = [
     "zombie",
     "ghoul",
@@ -13,14 +12,33 @@ var wordList = [
     "ghast",
     "shadow"
 ]
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var zombie = ["z", "o", "m", "b", "i", "e"]
+var ghoul = ["g", "h", "o", "u", "l"]
+var skeleton = ["s", "k", "e", "l", "e", "t", "o", "n"]
+var ghost = ["g", "h", "o", "s", "t"]
+var wraith = ["w", "r", "a", "i", "t", "h"]
+var lich = ["l", "i", "c", "h"]
+var poltergeist = ["p", "o", "l", "t", "e", "r", "g", "e", "i", "s", "t"]
+var shadow = ["s", "h", "a", "d", "o", "w"]
+var ghast = ["g", "h", "a", "s", "t"]
+var wight = ["w", "i", "g", "h", "t"]
 
-document.onkeyup = function(event) {
-    for (var i = 0; i < wordList.length; i++) {
-        // console.log(wordList[i])
-        // var letter = event
-        // console.log(letter)
-        if (wordList[0].charAt[i] == event) {
-            console.log(wordList[0].charAt[i])
+
+document.onkeyup = function (event) {
+    var key = event.key
+    if (alpha.indexOf(key.toLowerCase()) == '-1') {
+        alert("Making the code to check for your keys made me cry.");
+    }
+    else {
+    console.log(key)
+    for (var i = 0; i < zombie.length; i++) {
+        if (zombie[i].charAt(0) == key) {
+            console.log(zombie[i])
+
         }
     }
+    var paragraph = document.getElementById("guessed");
+    paragraph.textContent += key + ", ";
+}
 }
